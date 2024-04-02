4132875ebc9c8700db8f3ebcd68c29778bc930f6
lranatunga <rmlik.ranatunga@gmail.com>
2024-04-02 16:25:11 +0100

    social logins

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import SignupForm from './signup';
+import SocialLogins from './sociallogin';
 
 function App() {
   return (
     <div className="App">
       <SignupForm/>
+      <SocialLogins/>
     </div>
   );
 }
